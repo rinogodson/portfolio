2aac4ec2693ca0e28305debd818f1f755eb36846
Rino Godson <rinogodson@gmail.com>
2026-04-07 22:51:37 +0530

    Added the main page...

---
+++ astro.config.mjs
@@ -0,0 +1,19 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import react from '@astrojs/react';
+
+import tailwindcss from '@tailwindcss/vite';
+
+import icon from 'astro-icon';
+
+import svelte from '@astrojs/svelte';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [react(), icon(), svelte()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
---
+++ src/lib/motion-core/index.ts
@@ -0,0 +1,1 @@
+export { default as RubiksCube } from "./rubiks-cube/RubiksCube.svelte";
